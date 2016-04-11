52ca57dae5e6ed97a8dbc3c0811afc3e666de982
Ryan <ryan@freesimpleapps.com>
2016-04-10 20:04:18 -0400

    Initial Commit

---
+++ NET_Server_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NET_Server_Demo.WebApiApplication" Language="C#" %>
---
+++ NET_Server_Demo/Services/ContactRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NET_Server_Demo.Models;
+
+namespace NET_Server_Demo.Services
+{
+    public class ContactRepository
+    {
+        private string CacheKey = "ContactStore";
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact
+                        {
+                            Id = 1, Name = "Glenn Block"
+                        },
+                        new Contact
+                        {
+                            Id = 2, Name = "Dan Roth"
+                        }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
+            return new Contact[]
+                {
+                    new Contact
+                    {
+                        Id = 0,
+                        Name = "Placeholder"
+                    }
+                };
+        }
+    }
+}
