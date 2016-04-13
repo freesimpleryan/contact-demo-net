4948a8803359d64a38a0fdb4eeaf48c6f34999d7
Ryan <ryan@freesimpleapps.com>
2016-04-12 22:12:42 -0400

    Added validation rules server side

--- NET_Server_Demo/Controllers/ContactController.cs
+++ NET_Server_Demo/Controllers/ContactController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using NET_Server_Demo.Models;
 using NET_Server_Demo.Services;
+using System.Text.RegularExpressions;
 
 namespace NET_Server_Demo.Controllers
 {
@@ -25,14 +26,24 @@ namespace NET_Server_Demo.Controllers
 
         public HttpResponseMessage Post(Contact contact)
         {
-            if (contact.Id == null || contact.Id == "") {
-                contact.Id = Guid.NewGuid().ToString();
+            contact.Id = Guid.NewGuid().ToString();
+            Regex name = new Regex("[a-z|A-Z]{3,}");
+            Regex email = new Regex("[a-z|A-Z|0-9]{3,}@[a-z|A-Z|0-9]{3,}\\.[a-z|A-Z|0-9]{3,}");
+            Regex phone = new Regex("[2-9]{1}[0-9]{9}");
+            if (name.IsMatch(contact.FirstName) &&
+                name.IsMatch(contact.LastName) &&
+                email.IsMatch(contact.Email) &&
+                phone.IsMatch(contact.PhoneNumber)
+                )
+            {
+                this.contactRepository.SaveContact(contact);
+                var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+                return response;
             }
-            this.contactRepository.SaveContact(contact);
-
-            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
-
-            return response;
+            else {
+                var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.NotAcceptable, contact);
+                return response;
+            }
         }
     }
 }
